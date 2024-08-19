816a17e350f8106980a741a3718ae59811423cdf
thiago morgado <thiago_morgado@hotmail.com>
2024-08-19 10:15:20 -0300

    Initial components Card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,6 @@
+import { View } from "react-native";
+import { Container } from "./styles";
+
+export default function Card() {
+  return <Container></Container>;
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,35 +2,14 @@ import { Text, View } from "react-native";
 import { Container, Title } from "./styles";
 import Header from "@components/Header";
 import Typography from "@components/Typography";
+import Card from "@components/Card";
 
 export default function Home() {
   return (
     <Container>
       <Header />
-      <Typography
-        textAlign="right"
-        fontFamily="bold"
-        color="green_dark"
-        lineHeight="lg"
-      >
-        Ola
-      </Typography>
-      <Typography
-        textAlign="center"
-        fontFamily="regular"
-        color="red_dark"
-        lineHeight="lg"
-      >
-        Ola
-      </Typography>
-      <Typography
-        textAlign="left"
-        fontFamily="bold"
-        color="green_mid"
-        lineHeight="lg"
-      >
-        Ola
-      </Typography>
+
+      <Card />
     </Container>
   );
 }
