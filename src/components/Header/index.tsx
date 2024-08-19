225ce7e2533b3966143c1486faf8b3ea3250523d
thiago morgado <thiago_morgado@hotmail.com>
2024-08-19 05:50:31 -0300

    reconfigurations app.json upload appIcon and splashscreen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,8 +7,6 @@ export default function Header() {
     <Container>
       <Avatar />
       <Logo />
-
-     
     </Container>
   );
 }
--- src/themes/index.ts
+++ src/themes/index.ts
@@ -28,6 +28,7 @@ export default {
 			xs: 12,
 			sm: 14,
 			md: 16,
+			lg: 18,
 		},
 		title: {
 			xs: 14,
