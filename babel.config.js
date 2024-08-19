adf349a0a6e3bbe0d7570069bbfd67175c77e4bd
thiago morgado <thiago_morgado@hotmail.com>
2024-08-19 02:37:11 -0300

    configure base project

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,21 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    "plugins": [
+      'module.exports', {
+        'root': ['./src'],
+        // Configuration or aliases for plugins
+        'alias': {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@dtos': './src/dtos',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@storage': './src/storage',
+          '@theme': './src/theme',
+          '@utils': './src/utils',      
+        }
+      }
+    ]
   };
 };
