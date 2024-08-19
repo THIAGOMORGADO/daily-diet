dda5a40cb60cb5b89909fd2908a9472b0926d8e1
thiago morgado <thiago_morgado@hotmail.com>
2024-08-19 03:08:27 -0300

    configure babel and tsconfig

--- App.tsx
+++ App.tsx
@@ -2,6 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans'
+import { ThemeProvider } from 'styled-components';
+
+// importando as customizacoes do tema
+import themes from 'src/themes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -9,10 +13,13 @@ export default function App() {
     NunitoSans_700Bold,
   })
   return (
+  <ThemeProvider theme={themes}>
     <View style={styles.container}>
-      {fontsLoaded ? <Text>Fons Carregado com sucesso...</Text> : <Text>Fonts nao Carregadas </Text>} 
+      {!fontsLoaded ? <Text>Fons Carregado com sucesso...</Text> : <Text>Fonts nao Carregadas </Text>} 
       <StatusBar style="auto" />
     </View>
+  </ThemeProvider>  
+   
   );
 }
 
