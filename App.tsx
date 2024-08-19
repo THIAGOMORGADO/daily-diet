d80e3f5a86fe79643cfe06c2396067e27ac68865
thiago morgado <thiago_morgado@hotmail.com>
2024-08-19 02:58:38 -0300

    loading app resolver probles

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans'
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+  })
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {fontsLoaded ? <Text>Fons Carregado com sucesso...</Text> : <Text>Fonts nao Carregadas </Text>} 
       <StatusBar style="auto" />
     </View>
   );
--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,24 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    "plugins": [
-      'module.exports', {
-        'root': ['./src'],
-        // Configuration or aliases for plugins
-        'alias': {
-          '@assets': './src/assets',
-          '@components': './src/components',
-          '@dtos': './src/dtos',
-          '@routes': './src/routes',
-          '@screens': './src/screens',
-          '@storage': './src/storage',
-          '@theme': './src/theme',
-          '@utils': './src/utils',      
-        }
-      }
-    ]
-  };
-};
+module.exports = function (api) {
+	api.cache(true)
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				'module-resolver',
+				{
+					root: ['./src'],
+					alias: {
+						'@assets': './src/assets',
+						'@components': './src/components',
+						'@dtos': './src/dtos',
+						'@routes': './src/routes',
+						'@screens': './src/screens',
+						'@storage': './src/storage',
+						'@theme': './src/theme',
+						'@utils': './src/utils',
+					},
+				},
+			],
+		],
+	}
+}
